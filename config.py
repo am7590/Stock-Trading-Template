a58eb2754232208491b066519e2868f19fd49ed3
Alek Michelson <am7590@rit.edu>
2024-02-13 21:21:17 -0500

    config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Do NOT push this to github!
 
 ALPACA_CREDS = {
-    "API_KEY": "PK9TT3E7I5GSAHGDOYRR",
-    "API_SECRET": "ZyVLHiikQ3bw5IGFy5eSNZATdcVg4dG1aqQ9QsaZ",
+    "API_KEY": "...",
+    "API_SECRET": "...",
     "PAPER": True
 }
